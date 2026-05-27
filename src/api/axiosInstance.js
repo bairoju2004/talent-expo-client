d7589d03cde6fa39dd911665f6dd96f471a6e16e
Bairoju Rajasri <bairojurajasri@gmail.com>
2026-05-27 09:07:57 +0530

    fix: update baseURL to production backend

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://talent-expo.onrender.com',
+  baseURL: 'https://talent-expo.onrender.com/api',
 });
 
 axiosInstance.interceptors.request.use((config) => {
